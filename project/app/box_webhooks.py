251dfffa54bcf8fd5e7e9668bf441b574a1ce948
Barduino <barduinor@gmail.com>
2023-01-12 00:34:14 +0000

    validate webhooks

--- project/app/box_webhooks.py
+++ project/app/box_webhooks.py
@@ -1,7 +1,6 @@
 """ handle box webhooks from box exvents"""
 from functools import lru_cache
 from sqlalchemy.orm import Session
-from boxsdk import BoxAPIException
 
 from app.config import Settings
 from app.box_jwt import jwt_check_client
@@ -10,7 +9,7 @@ from app.box_jwt import jwt_check_client
 @lru_cache()
 def webhook_check(webhook_id, client):
     """check if webhook exists for this application"""
-    
+
     webhook = client.webhook(webhook_id)
     # this raises an error if the webhook does not exist
     # or is not accessible on this security context
@@ -25,7 +24,7 @@ def webhook_signature_check(
     settings: Settings,
 ) -> bool:
     """check the signature of the webhook request"""
-    
+
     client = jwt_check_client(db, settings)
     webhook = webhook_check(webhook_id, client)
     key_a = settings.WH_KEY_A
